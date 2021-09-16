2bee13759d77ec42b0ac82a4701a888db3b6573d
Guo-Haowei <ghwhotline@gmail.com>
2021-09-16 10:40:22 +0800

    winit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use winit::event::VirtualKeyCode;
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
+
+fn main() {
+    let mut input = WinitInputHelper::new();
+
+    let event_loop = EventLoop::new();
+    let _window = WindowBuilder::new()
+        .with_resizable(false)
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        // Pass every event to the WindowInputHelper.
+        // It will return true when the last event has been processed and it is time to run your application logic.
+        if input.update(&event) {
+            // query keypresses this update
+            if input.key_pressed(VirtualKeyCode::A) {
+                println!("The 'A' key was pressed on the keyboard");
+            }
+
+            if input.key_released(VirtualKeyCode::Q) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            // query the change in mouse this update
+            let mouse_diff = input.mouse_diff();
+            if mouse_diff != (0.0, 0.0) {
+                println!("The mouse diff is: {:?}", mouse_diff);
+                println!("The mouse position is: {:?}", input.mouse());
+            }
+
+            // You are expected to control your own timing within this block.
+            // Usually via rendering with vsync.
+            // render();
+        }
+    });
+}
